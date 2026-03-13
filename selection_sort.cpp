e3f3429968a64516fea764863c9c647a5c64c5e3
rohanchouhan07 <rohanchouhan.sage@gmail.com>
2026-03-13 13:59:13 +0530

    rohan

--- selection_sort.cpp
+++ selection_sort.cpp
@@ -1,9 +1,12 @@
 #include<iostream>
 using namespace std;
 int main(){
-    int n=5;
-    // cin>n;
-    int a[n]={5,4,3,2,1};
+    int n;
+    cin>>n;
+    int a[n];
+    for(int i=0;i<n;i++){
+    cin>>a[i];
+    }
     
     int i=0;
     int min=a[0];
@@ -11,18 +14,23 @@ int main(){
     for(int i=0;i<n;i++){
         min=a[i];
         int pivot=i;
-        for(int j=i+1;j<n;j++){
+        for(int j=i+1;j<=n;j++){
             if(min>a[j]){
                 min=a[j];
                 pivot=j;
             }
         }
         swap(a[i],a[pivot]);
+        /*
+        int temp=a[i];
+        a[i]=a[pivot];
+        a[pivot]=temp;
+        */
 
     }
 
     for(int i=0;i<n;i++){
-        cout<<a[i];
+        cout<<a[i]<<"  ";
     }
 
 }
