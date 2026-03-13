421442d3a44acf8cd81e77ded2de12a0052f51a6
rohanchouhan07 <rohanchouhan.sage@gmail.com>
2026-03-13 12:12:51 +0530

    r

--- binary_scr.cpp
+++ binary_scr.cpp
@@ -1,16 +1,19 @@
 #include<iostream>
+#include<conio.h>
 using namespace std;
 int main(){
+
     int n;
+    int t;
+    
     cout<<"enter range";
     cin>>n;
     int arr[n];
     for(int i=0;i<n;i++){
         cin>>arr[i];
     }
-
+    
     int l=0,r=n-1;
-    int t;
 
     cout<<"enter num to search";
     cin>>t;
@@ -22,11 +25,11 @@ int main(){
             cout<<"num found at"<<mid+1;
             break;
         }
-        else if(arr[mid] < t ){
-            l=mid+1;
-        }
-        else if(arr[mid] > t){
+        else if(arr[mid] > t ){
             r=mid-1;
+        }
+        else if(arr[mid] < t){
+            l=mid+1;
         }
     }
 if(l>=r){
